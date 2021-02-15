e70854a1102d9c9c5e8436cc52806c0149f6eaca
GitAntinus <gantinus@gmail.com>
2021-02-15 17:32:16 +0800

    完善Asset模型：增加现金和锁定的概念与操作
    Signed-off-by: GitAntinus <gantinus@gmail.com>

--- domain/src/main/java/org/example/trade/domain/account/Asset.java
+++ domain/src/main/java/org/example/trade/domain/account/Asset.java
@@ -1,13 +1,53 @@
 package org.example.trade.domain.account;
 
+import org.example.trade.domain.market.Money;
+import org.example.trade.domain.market.StockCode;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
 
 public class Asset {
 
     private final Id id;
 
-    public Asset(Id id) {
+    private final Map<StockCode, Position> positions;
+
+    private Money usableCash;
+
+    private Money lockedCash;
+    // TODO
+
+    public Asset(Id id, Money lockedCash,
+                 Map<StockCode, Position> positions, Money usableCash) {
         this.id = id;
+        this.lockedCash = lockedCash;
+        this.positions = positions;
+        this.usableCash = usableCash;
+    }
+
+    public Money usableCash() {
+        return usableCash;
+    }
+
+    public Money lockedCash() {
+        return lockedCash;
+    }
+
+    /**
+     * 锁定一部分现金，使得它们不再可以被使用为其他目的
+     *
+     * @param amount 要锁定的数量
+     * @return 是否锁定成功，如果需要的数量小于可用的现金，则可以锁定，反之不行
+     */
+    public synchronized boolean lockCash(Money amount) {
+        if (usableCash.compareTo(amount) < 0) { return false; }
+        lockedCash = lockedCash.add(amount);
+        usableCash = usableCash.subtract(amount);
+        return true;
+    }
+
+    public Map<StockCode, Position> positions() {
+        return positions;
     }
 
     public Id id() {
--- domain/src/main/java/org/example/trade/domain/account/Position.java
+++ domain/src/main/java/org/example/trade/domain/account/Position.java
@@ -1,4 +1,5 @@
 package org.example.trade.domain.account;
 
 public class Position {
+    // TODO
 }
