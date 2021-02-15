46df1cee7de81457d8885a57e0c39946f513be8b
GitAntinus <gantinus@gmail.com>
2021-02-16 01:29:58 +0800

    新建一个实现组件broker trade service，用于实现TradeService，而且是作为父类为所有的可能实现提供通用的处理片段，这部分涉及到通用的对领域模型的调用和存储。
    Signed-off-by: GitAntinus <gantinus@gmail.com>

---
+++ broker-trade-service/src/main/java/org/example/trade/infrastructure/broker/BrokerTradeService.java
@@ -0,0 +1,34 @@
+package org.example.trade.infrastructure.broker;
+
+import org.example.trade.domain.market.Broker;
+import org.example.trade.domain.market.Price;
+import org.example.trade.domain.market.Shares;
+import org.example.trade.domain.order.Deal;
+import org.example.trade.domain.order.TradeOrder;
+import org.example.trade.domain.order.TradeOrderRepository;
+import org.example.trade.domain.order.TradeService;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Instant;
+
+public abstract class BrokerTradeService extends TradeService {
+
+    protected final TradeOrderRepository tradeOrderRepository;
+
+    protected BrokerTradeService(TradeOrderRepository tradeOrderRepository) {
+        this.tradeOrderRepository = tradeOrderRepository;
+    }
+
+    protected void handleTradeEvent(String broker, String orderId, BigInteger shares, BigDecimal price, Instant time) {
+        TradeOrder.Id id = new TradeOrder.Id(new Broker(broker), orderId, true);
+        TradeOrder order = tradeOrderRepository.findById(id);
+        Deal deal = new Deal(
+            new Shares(shares),
+            new Price(price)
+        );
+        order.makeDeal(deal, time);
+        tradeOrderRepository.save(order);
+    }
+
+}
