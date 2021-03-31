568db1e3960c1c8402cbfb245ef76c05183f8c08
Eric Deng <gantinus@gmail.com>
2021-03-31 12:39:15 +0800

    使用有序消息系统后，不再需要对同一个对象的事件处理器进行并发加锁来排序
    Signed-off-by: Eric Deng <gantinus@gmail.com>

--- application/src/main/java/org/example/trade/application/AssetService.java
+++ application/src/main/java/org/example/trade/application/AssetService.java
@@ -16,11 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 @Service
 public class AssetService {
 
@@ -29,8 +24,6 @@ public class AssetService {
     private final AssetRepository assetRepository;
 
     private final SingleAccountBrokerServiceFactory factory;
-
-    private final ConcurrentHashMap<OrderId, ReadWriteLock> handlingLocks = new ConcurrentHashMap<>();
 
     @Autowired
     public AssetService(AssetRepository assetRepository,
@@ -54,46 +47,33 @@ public class AssetService {
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     private boolean handleOrderClosed(OrderClosed orderEvent) {
         OrderId orderId = orderEvent.orderId();
-        Lock writeLock = handlingLocks.computeIfAbsent(orderId, o -> new ReentrantReadWriteLock()).writeLock();
-        try {
-            logger.info("尝试开始处理close: {}", orderId.uid());
-            writeLock.lock();
-            logger.info("开始处理close: {}", orderId.uid());
-            logger.info("尝试获取asset锁: {}", orderId.uid());
-            Asset asset = assetRepository.findById(orderId.accountId());
-            logger.info("获取asset锁 {}", orderId.uid());
-            asset.reclaim(orderId);
-            assetRepository.save(asset);
-            logger.info("回收订单{} 的资源", orderId.uid());
-            handlingLocks.remove(orderId);
-            DomainEventBus.instance().publish(asset);
-        } finally {
-            writeLock.unlock();
-        }
+        logger.info("尝试开始处理close: {}", orderId.uid());
+        logger.info("开始处理close: {}", orderId.uid());
+        logger.info("尝试获取asset锁: {}", orderId.uid());
+        Asset asset = assetRepository.findById(orderId.accountId());
+        logger.info("获取asset锁 {}", orderId.uid());
+        asset.reclaim(orderId);
+        assetRepository.save(asset);
+        logger.info("回收订单{} 的资源", orderId.uid());
+        DomainEventBus.instance().publish(asset);
         return true;
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     private boolean handleOrderTraded(OrderTraded orderEvent) {
         OrderId orderId = orderEvent.orderId();
-        Lock readLock = handlingLocks.computeIfAbsent(orderId, o -> new ReentrantReadWriteLock()).readLock();
-        try {
-            logger.info("尝试开始处理trade: {}", orderId.uid());
-            readLock.lock();
-            logger.info("开始处理trade: {}", orderId.uid());
-            logger.info("尝试获取asset锁: {}", orderId.uid());
-            Asset asset = assetRepository.findById(orderId.accountId());
-            logger.info("获取asset锁 {}", orderId.uid());
-            boolean overDealt = !asset.consume(orderId, orderEvent.deal());
-            assetRepository.save(asset);
-            logger.info("asset updated for {}", orderId.uid());
-            if (overDealt) {
-                logger.warn("券商交易数量超过分配数量，{} 现有资源: {}", orderId.uid(), asset.resourceOf(orderId));
-            }
-            DomainEventBus.instance().publish(asset);
-        } finally {
-            readLock.unlock();
+        logger.info("尝试开始处理trade: {}", orderId.uid());
+        logger.info("开始处理trade: {}", orderId.uid());
+        logger.info("尝试获取asset锁: {}", orderId.uid());
+        Asset asset = assetRepository.findById(orderId.accountId());
+        logger.info("获取asset锁 {}", orderId.uid());
+        boolean overDealt = !asset.consume(orderId, orderEvent.deal());
+        assetRepository.save(asset);
+        logger.info("asset updated for {}", orderId.uid());
+        if (overDealt) {
+            logger.warn("券商交易数量超过分配数量，{} 现有资源: {}", orderId.uid(), asset.resourceOf(orderId));
         }
+        DomainEventBus.instance().publish(asset);
         return true;
     }
 
